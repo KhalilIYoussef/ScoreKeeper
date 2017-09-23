374435608c9028d3aa74904891c559594dd17964
khaliliyoussef <khalilibrahim865@gmail.com>
2017-09-23 15:02:26 -0700

    fix: some issues

--- app/src/main/java/khaliliyoussef/scorekeeper/MainActivity.java
+++ app/src/main/java/khaliliyoussef/scorekeeper/MainActivity.java
@@ -1,9 +1,28 @@
 package khaliliyoussef.scorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+    //team one
+    @BindView(R.id.buttonTeamOneAddFoul) Button buttonTeamOneAddFoul;
+    @BindView(R.id.buttonTeamOneAddPoint) Button buttonTeamOneAddPoint;
+    @BindView(R.id.textViewTeamOneScore) TextView ScoreTeamOne;
+    @BindView(R.id.textViewTeamOneFoulNumber) TextView textViewTeamOneFoul;
+
+    //team two
+    @BindView(R.id.buttonTeamTwoAddPoint) Button buttonTeamTwoAddPoint;
+    @BindView(R.id.buttonTeamTwoAddFoul) Button buttonTeamTwoAddFoul;
+    @BindView(R.id.textViewTeamTwoScore) TextView ScoreTeamTwo;
+    @BindView(R.id.textViewTeamTwoFoulNumber) TextView textViewTeamTwoFoul;
+
+    //rest button
+    @BindView(R.id.buttonReset) Button buttonReset;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
